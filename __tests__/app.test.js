9a0bddfd360640260231bb60dc6fe4694b6ddbf2
Elise Muellerleile <eliamue@live.com>
2022-02-18 22:46:22 -0600

    require syntax

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-import Resources from '../lib/models/Resources.js';
+const { Resources } = require('../lib/models/Resources.js');
 describe('Resources CRUD routes', () => {
   beforeEach(() => {
     return setup(pool);
