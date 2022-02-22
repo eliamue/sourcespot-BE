7d21921c0d1f0b9c4514639f3bccd708f1b27e54
Elise Muellerleile <eliamue@live.com>
2022-02-22 00:32:41 -0600

    tags now post

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,14 @@
-DROP TABLE IF EXISTS resources CASCADE;
 DROP TABLE IF EXISTS tags CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
 DROP TABLE IF EXISTS locations CASCADE;
 DROP TABLE IF EXISTS resource_tags CASCADE;
 DROP TABLE IF EXISTS resource_locations CASCADE;
+
+CREATE TABLE tags (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    tag TEXT,
+    UNIQUE(id)
+);
 
 CREATE TABLE resources (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -10,13 +16,10 @@ CREATE TABLE resources (
     category TEXT NOT NULL,
     about TEXT NOT NULL,
     website TEXT,
-    logo TEXT
+    logo TEXT,
+    UNIQUE(id)
 );
 
-CREATE TABLE tags (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    tag TEXT
-);
 
 CREATE TABLE locations (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -25,7 +28,10 @@ CREATE TABLE locations (
 
 CREATE TABLE resource_tags (
     tagid BIGINT NOT NULL REFERENCES tags(id),
-    resourceid BIGINT NOT NULL REFERENCES resources(id)
+    resourceid BIGINT NOT NULL REFERENCES resources(id),
+    PRIMARY KEY (tagid, resourceid),
+    FOREIGN KEY (tagid) REFERENCES resources(id) ON DELETE CASCADE
+
 );
 
 CREATE TABLE resource_locations (
