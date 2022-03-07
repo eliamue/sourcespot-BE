ab21205b4f7040c4d8fca6ed5e6682bd0cdf2636
Elise Muellerleile <eliamue@live.com>
2022-03-06 19:45:15 -0600

    SQL tables added for categories, tags, locations, and join tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,40 +1,42 @@
+DROP TABLE IF EXISTS categories CASCADE;
 DROP TABLE IF EXISTS tags CASCADE;
-DROP TABLE IF EXISTS resources CASCADE;
+DROP TABLE IF EXISTS category_tags CASCADE;
 DROP TABLE IF EXISTS locations CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
 DROP TABLE IF EXISTS resource_tags CASCADE;
 DROP TABLE IF EXISTS resource_locations CASCADE;
 
+CREATE TABLE categories (
+    category_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    category TEXT NOT NULL
+);
+
 CREATE TABLE tags (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    tag TEXT,
-    UNIQUE(id)
+    tag_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    category_id INT NOT NULL REFERENCES categories(category_id) 
+    tag TEXT
 );
 
 CREATE TABLE resources (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    resource_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    category_id INT NOT NULL REFERENCES categories(category_id),
     title TEXT NOT NULL,
-    category TEXT NOT NULL,
     about TEXT NOT NULL,
     website TEXT,
-    logo TEXT,
-    UNIQUE(id)
-);
-
-
-CREATE TABLE locations (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    located TEXT
+    logo TEXT
 );
 
 CREATE TABLE resource_tags (
-    tagid BIGINT NOT NULL REFERENCES tags(id),
-    resourceid BIGINT NOT NULL REFERENCES resources(id),
-    PRIMARY KEY (tagid, resourceid),
-    FOREIGN KEY (tagid) REFERENCES resources(id) ON DELETE CASCADE
+    tag_id INT NOT NULL REFERENCES tags(tag_id),
+    resource_id INT NOT NULL REFERENCES resources(resource_id)
+);
 
+CREATE TABLE locations (
+    location_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    located TEXT
 );
 
 CREATE TABLE resource_locations (
-    locationid BIGINT NOT NULL REFERENCES locations(id),
-    resourceid BIGINT NOT NULL REFERENCES resources(id)
+    location_id INT NOT NULL REFERENCES locations(location_id),
+    resource_id INT NOT NULL REFERENCES resources(resource_id)
 );
